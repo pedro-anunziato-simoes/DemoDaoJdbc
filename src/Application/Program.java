b9bb3324791ad819aefcce9011af8bec742f6c44
User <pedro.anunziato.simoes@gmail.com>
2024-07-15 15:58:48 -0300

    inset implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -29,6 +29,10 @@ public class Program {
 		for (Seller obj : list) {
 				System.out.println(obj);
 		}
-
+		
+		System.out.println("\n=== Test3: Seller Insert===");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+newSeller.getId());
 	}
 }
